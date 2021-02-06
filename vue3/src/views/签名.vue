81052cec728ecb048c286afe50b53566b1cdb661
KiritaniMirei <chenmxxfs@xinfushe.com>
2021-02-07 00:02:25 +0800

    canvas签名最多可以会退3步 可以配置

--- vue3/src/views/签名.vue
+++ vue3/src/views/签名.vue
@@ -11,12 +11,18 @@
         <van-button plain
           type="primary"
           round
-          @click="clearCanvas">上一步
+          :class="{disabled: preDisabled}"
+          @click="prev">上一步
+        </van-button>
+        <van-button plain
+          type="primary"
+          round
+          @click="next">下一步
         </van-button>
         <van-button plain
           type="success"
           round
-          @click="clearCanvas">确定保存
+          @click="clearCanvas"> 保存
         </van-button>
       </div>
     </div>
@@ -24,28 +30,58 @@
 </template>
 
 <script lang='ts'>
-import { defineComponent, onMounted } from 'vue';
+import { computed, defineComponent, onMounted, ref, watch } from 'vue';
 
 export default defineComponent({
   setup() {
     let canvas: HTMLCanvasElement | null, ctx: CanvasRenderingContext2D | null;
+    const imageDataArr = ref<any>([]);
+    const currentIndex = ref(0);
+    const ratio = 3;
+    const maxMemo = 3;
+    const imgData = computed(() => {
+      if (currentIndex.value >= 0 && currentIndex.value < imageDataArr.value.length) {
+        return imageDataArr.value[currentIndex.value];
+      }
+    });
+    const preDisabled = computed(() => {
+      return (imageDataArr.value.length < 4 && currentIndex.value < 0) || (imageDataArr.value.length >= 4 && currentIndex.value === 0);
+    });
     const clearCanvas = () => {
+      imageDataArr.value = [];
       if (canvas && ctx) {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
       }
     };
+    const prev = () => {
+      if (canvas && ctx) {
+        if (!preDisabled.value) {
+          currentIndex.value--;
+        }
+      }
+    };
+    watch(currentIndex, () => {
+      if (canvas && ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        if (imgData.value) {
+          ctx.putImageData(imgData.value, 0, 0);
+        }
+      }
+    });
+    const next = () => {};
     onMounted(() => {
       canvas = document.querySelector('canvas');
       const clientWidth = Math.min(document.documentElement.clientWidth, 480);
       if (canvas) {
-        canvas.width = canvas.height = Math.floor((clientWidth * 600) / 750);
+        canvas.width = canvas.height = Math.floor((clientWidth * 600 * ratio) / 750);
         ctx = canvas.getContext('2d');
         if (ctx) {
           ctx.fillStyle = '#fff';
           ctx.fillRect(0, 0, canvas.width, canvas.height);
           ctx.strokeStyle = '#111';
-          ctx.lineWidth = 10;
+          ctx.lineWidth = 10 * ratio;
           ctx.lineCap = 'round';
+          ctx.lineJoin = 'round';
         }
         const offsetY = canvas.getBoundingClientRect().top;
         const offsetX = canvas.getBoundingClientRect().left;
@@ -53,19 +89,22 @@ export default defineComponent({
           e.preventDefault();
           if (ctx) {
             ctx.beginPath();
-            ctx.moveTo(e.targetTouches[0].pageX - offsetX, e.targetTouches[0].pageY - offsetY);
+            ctx.moveTo((e.targetTouches[0].pageX - offsetX) * ratio, (e.targetTouches[0].pageY - offsetY) * ratio);
           }
         });
         canvas.addEventListener('touchmove', (e: TouchEvent) => {
           e.preventDefault();
           if (ctx) {
-            ctx.lineTo(e.targetTouches[0].pageX - offsetX, e.targetTouches[0].pageY - offsetY);
+            ctx.lineTo((e.targetTouches[0].pageX - offsetX) * ratio, (e.targetTouches[0].pageY - offsetY) * ratio);
             ctx.stroke();
           }
         });
         canvas.addEventListener('touchend', () => {
-          if (ctx) {
-            console.log(ctx.getImageData(0, 0, (canvas as HTMLCanvasElement).width, (canvas as HTMLCanvasElement).height));
+          if (canvas && ctx) {
+            const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            imageDataArr.value.push(data);
+            imageDataArr.value = imageDataArr.value.slice(maxMemo * -1 - 1);
+            currentIndex.value = imageDataArr.value.length - 1;
             ctx.closePath();
           }
         });
@@ -74,6 +113,12 @@ export default defineComponent({
 
     return {
       clearCanvas,
+      prev,
+      next,
+      imageDataArr,
+      imgData,
+      currentIndex,
+      preDisabled,
     };
   },
 });
@@ -83,10 +128,19 @@ export default defineComponent({
 .sign
   height 100vh
   flex-center()
+  &>div
+    flex 1
+    margin 0 20px
   canvas
     border 1px solid #ddd
     display block
+    width 600px
+    height 600px
+    margin 0 auto
   &>div>div
-    margin-top 100px
+    margin-top 200px
     flex-space()
+  button.disabled
+    opacity 0.2
+    pointer-events none
 </style>
